8ce3ee2276b98ebc9bb5be868496c16b6a224023
Steve Grogorick <steve@grogorick.de>
2019-09-25 19:37:36 +0200

    removed hardcoded URL part

--- link/index.php
+++ link/index.php
@@ -42,7 +42,7 @@ else {
 	if (isset($_GET["add"])) {
 ?>
 		
-		<form action="/link/" method="post">
+		<form action="<?=$_SERVER['REQUEST_URI']?>" method="post">
 			<div style="display: inline-block;">Name: <input type="text" name="name" required pattern="[a-zA-Z0-9\-]+" placeholder="Nur Buchstaben und Zahlen" style="width: 200px;" /> &nbsp;</div>
 			<div style="display: inline-block;">URL: <input type="text" name="url" required placeholder="https://#####.synology.me/photo/share/" style="width: 400px;" /> &nbsp;</div>
 			<input type="submit" name="add" value="Speichern" />
@@ -50,18 +50,17 @@ else {
 		
 <?php
 	}
-
 	foreach ($urls as $key => $url) {
 		if (isset($_GET["delete"])) {
 ?>
-		<form action="/link/" method="post" style="display: inline;">
+		<form action="<?=$_SERVER['REQUEST_URI']?>" method="post" style="display: inline;">
 			<input type="hidden" name="name" value="<?=$key?>" />
 			<input type="submit" name="delete" value="X" onclick="return confirm('<?=$key?>\nwirklich löschen?');" />
 		</form>
 <?php
 		}
 ?>
-		<a href="https://#####.synology.me/link/?<?=$key?>"><?=$key?></a> &nbsp; &#x21E2; &nbsp; <?=$url?><br />
+		<a href="https://link.yournicedyndnsdomain.com/?<?=$key?>"><?=$key?></a> &nbsp; &#x21E2; &nbsp; <?=$url?><br />
 <?php
 	}
 }
